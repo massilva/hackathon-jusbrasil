504a34af1665d9225bbb2b82b3f4a34e789c91c0
Victor dos Santos <vitocr_santos@hotmail.com>
2014-01-16 17:10:58 -0300

    Progredindo na refatoração

--- controllers/empresa.php
+++ controllers/empresa.php
@@ -1,6 +1,5 @@
 <?php 	
 
-include_once "../vendors/swift/lib/swift_required.php";
 require_once '../vendors/Twig/lib/Twig/Autoloader.php';
 require_once "../model/empresaModel.php";
 
@@ -8,17 +7,33 @@ Twig_Autoloader::register();
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
-// $loader = new Twig_Loader_String();
 $loader = new Twig_Loader_Filesystem('../view');
 $twig = new Twig_Environment($loader);
+
+$empresa = new empresaModel();
 
 if ($action === 'index')
 	index();
 
 function index()
 {
-	global $twig;
-	echo $twig->render('index.twig');
+	global $twig, $empresa;
+	$estados = array();
+	$orgaos;
+	$motivos = array();
+	$mot = $empresa->getMotivo();
+	$ranking = $empresa->getDetalheMotivo();
+
+	foreach($mot as $key => $m)
+	{
+		$motivos[$key]['label'] = utf8_encode($m->tipo_sancao);
+		$motivos[$key]['value'] = $m->qtd;
+	}
+
+	foreach($ranking as $r)
+		$r->tipo_sancao = utf8_encode($r->tipo_sancao);
+	
+	echo $twig->render('index.twig', array('estados' => $estados, 'orgaos' => $orgaos, 'motivos' => $motivos, 'ranking' => $ranking));
 }
 
 ?>
